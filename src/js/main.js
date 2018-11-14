aea2072e5721dd4393b1c47ab9ae4e000a1eec59
Igor Tarasenko <tarasenko.igr@gmail.com>
2018-11-14 10:15:41 +0200

    fixed bag with iframe in page 'calendar'

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
         $(".hover-block").removeClass('active');
         $(this).addClass('active');
     });
+    setTimeout(function(){
+        /*$("iframe#calendar").contents().find("#headerRow").hide();*/
+        var iFrameDOM = $("iframe #calendar").contents();
+        var count = iFrameDOM.find(".eCalNew header");
+        console.log(iFrameDOM.get());
+    }, 3000);
+    /*$('#headerRow:lt(3)').css('display', 'none');*/
 });
